e4314b9693f4e34a9b7921dd1825713f2c1386be
CynthiaOuma12673 <oumacynthia817@gmail.com>
2023-08-22 10:25:02 -0400

    add the regitration views to views.py

--- store/product/views.py
+++ store/product/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegistrationForm
 
 # Create your views here.
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method=="POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request,f'Account created for {username}!')
+            return redirect('login')
+            
+    else:
+        form = UserRegistrationForm()
+    return render(request,"registration/signup.html",{'form':form})
+
